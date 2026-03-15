b32dddab30529e3bcc72df5de3a03e2fa4cc139a
kenan <kenansharifov400@gmail.com>
2026-03-15 16:12:30 +0400

    feat

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,15 @@ const nextConfig: NextConfig = {
     "@mikro-orm/migrations",
     "@mikro-orm/cli",
   ],
-  turbopack: {},
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.optimization = {
+        ...config.optimization,
+        minimize: false,
+      };
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
